fd0abe8f3bc201229ba2f882e445eaf183426f9b
Juan Corredor <ca_juan@javeriana.edu.co>
2023-04-11 18:27:05 -0500

    Reemplazado video introductorio por imagen. Creado script para esto

---
+++ SpriteSW.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteSW : MonoBehaviour{
+
+    private GenSceneSW escena;
+    public Sprite intro;
+    public Image fondo;
+
+    void Start() {
+        escena = FindObjectOfType<GenSceneSW>();
+    }
+
+    public void SCambio(){
+        fondo.sprite = intro;
+        StartCoroutine(delay());
+    }
+
+    public IEnumerator delay(){
+        yield return new WaitForSeconds(2);
+        escena.CambioScene("Frontal");
+    }
+
+}
